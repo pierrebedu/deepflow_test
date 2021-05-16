54a0a909107936ae72a3a35f4efb5d59b7a787b0
pierre_bedu <pierrebedu@yahoo.fr>
2021-05-16 09:59:49 +0200

    canny detection working

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-print(2)
+""" kmeans clustering procedure"""
 
 original_image = cv2.imread("C:\\Users\\pierr\\Desktop\\os2.png")
 img=cv2.cvtColor(original_image,cv2.COLOR_BGR2RGB)
@@ -29,3 +29,18 @@ plt.title('Original Image'), plt.xticks([]), plt.yticks([])
 plt.subplot(1,2,2),plt.imshow(result_image)
 plt.title('Segmented Image when K = %i' % K), plt.xticks([]), plt.yticks([])
 plt.show()
+
+
+
+"""try to process the edge detection"""
+edges = cv2.Canny(img, 49, 55)
+plt.figure(figsize=(figure_size,figure_size))
+plt.subplot(1,2,1),plt.imshow(img)
+plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(1,2,2),plt.imshow(edges,cmap = 'gray')
+plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+plt.show()
+
+"""try to find the corner detection"""
+
+
