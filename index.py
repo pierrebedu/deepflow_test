80317c8cf22bc6ad7fd589c2d4185d8de35c414c
pierre_bedu <pierrebedu@yahoo.fr>
2021-05-16 09:41:49 +0200

    kmeans clustering working

--- index.py
+++ index.py
@@ -2,15 +2,30 @@
 
 """ explainations"""
 
-import math
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
 
-print(2*2)
+print(2)
 
-a=2*4
+original_image = cv2.imread("C:\\Users\\pierr\\Desktop\\os2.png")
+img=cv2.cvtColor(original_image,cv2.COLOR_BGR2RGB)
+vectorized = img.reshape((-1,3))
+vectorized = np.float32(vectorized)
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
 
-print(a)
+K = 8
 
-x=math.pi**2
+attempts=10
+ret,label,center=cv2.kmeans(vectorized,K,None,criteria,attempts,cv2.KMEANS_PP_CENTERS)
 
-print(x)
-
+center = np.uint8(center)
+res = center[label.flatten()]
+result_image = res.reshape((img.shape))
+figure_size = 15
+plt.figure(figsize=(figure_size,figure_size))
+plt.subplot(1,2,1),plt.imshow(img)
+plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(1,2,2),plt.imshow(result_image)
+plt.title('Segmented Image when K = %i' % K), plt.xticks([]), plt.yticks([])
+plt.show()
